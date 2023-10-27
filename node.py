eb29a1542b59722ca71ff028563a162783df4fa5
Eduardo M. Martins <105308925+Eduardo-M-Martins@users.noreply.github.com>
2023-10-27 14:20:20 -0300

    cores e prints

--- node.py
+++ node.py
@@ -19,12 +19,23 @@ ip_destny, last_msg, nickname = "", "", ""
 delay, token = 0, -1
 gen_token = False
 
-SOCKET = socket(AF_INET, SOCK_DGRAM)
-TOKEN = "9000"
-TOKEN_EXCESS = 1
-TOKEN_TIMEOUT = 15
-PORT = 5000
-ERRO = True
+SOCKET = socket(AF_INET, SOCK_DGRAM)    # Socket.
+TOKEN = "9000"                          # Define o token.
+TOKEN_EXCESS = 1                        # Tempo mínimo entre a chegada de Tokens.
+TOKEN_TIMEOUT = 15                      # Tempo máximo sem receber Tokens.
+PORT = 5000                             # Porta da conexão.
+PRINT = True                            # Define se os detalhes do Token e pacotes serão impressos no chat.
+ERRO = True                             # Define se o gerador de erros deve estar ativado.
+
+DARK_STYLE = "\033[1m\33[90m"
+END_STYLE = "\033[0m\033[0m"
+GLOBAL_STYLE = "\033[1m\33[96m"
+HEADER_STYLE = "\033[1m\33[95m"
+PRIV_STYLE = "\033[1m\033[94m"
+SYSTEM_STYLE = "\033[1m\33[31m"
+TEXT_STYLE = "\033[1m\33[37m"
+TITLE_STYLE = "\033[1m\33[32m"
+WARNING_STYLE = "\033[1m\033[93m"
 
 ##############################################################################################################
 # Funções:
@@ -52,7 +63,7 @@ def handle_token():
         if (datetime.now() - last_token_date).total_seconds() >= TOKEN_TIMEOUT:
             # Se não é o primeiro a ser gerado token:
             if last_token_date != datetime(1900, 1, 1, 0, 0):
-                print("SYSTEM: Token timeout. New token generated.")
+                print(SYSTEM_STYLE + "SYSTEM: Token timeout. New token generated." + END_STYLE)
             last_token_date = datetime.now()
             send(TOKEN)
         time.sleep(1)
@@ -72,8 +83,8 @@ def recive():
     
     SOCKET.bind(("0.0.0.0", PORT))
     
-    print("\n# === === === === === === Port === === === === === === #\n")
-    print("  → UDP: " + str(PORT))
+    print(HEADER_STYLE + "\n# === === === === === === Port === === === === === === #\n" + END_STYLE)
+    print(TITLE_STYLE + "    UDP: " + END_STYLE + DARK_STYLE + str(PORT) + END_STYLE)
     
     # Loop para ficar esperando por pacotes.
     while True:
@@ -87,6 +98,7 @@ def recive():
                 if (datetime.now() - last_token_date).total_seconds() >= TOKEN_EXCESS:
                     last_token_date = datetime.now()
                     token = TOKEN
+                    if PRINT: print(WARNING_STYLE + "Token Check: " + str(last_token_date.time()) + END_STYLE)
                     if len(message_list) > 0:
                         msg = message_list.pop(0)
                         send(msg)
@@ -96,6 +108,7 @@ def recive():
             
             # Se o pacote for uma menssagem:
             elif data.startswith("7777:"):
+                if PRINT: print(WARNING_STYLE + "Package Check: " + data + END_STYLE)
                 header = data.split(":")[1].split(";")[0]
                 from_nickname = data.split(":")[1].split(";")[1]
                 to_nickname = data.split(":")[1].split(";")[2]
@@ -104,7 +117,7 @@ def recive():
                 
                 # Se a menssagem é para todos:
                 if to_nickname == "TODOS":         
-                    print(from_nickname + " (global): " + msg)
+                    print(GLOBAL_STYLE + from_nickname + " (global): " + END_STYLE + TEXT_STYLE + msg + END_STYLE)
                     # Se não foi eu que mandei a menssagem:
                     if nickname != from_nickname:       
                         send(data)
@@ -120,11 +133,11 @@ def recive():
                         send("7777:NACK;" + from_nickname + ";" + to_nickname + ";" + crc + ";" + msg)
                     # Se não foi eu que mandei a menssagem:
                     elif nickname != from_nickname:       
-                        print(from_nickname + ": " + msg)
+                        print(PRIV_STYLE + from_nickname + ": " + END_STYLE + msg)
                         send("7777:ACK;" + from_nickname + ";" + to_nickname + ";" + crc + ";" + msg)
                     # Se foi eu que mandei a menssagem:
                     else:                               
-                        print(from_nickname + " (eu): " + msg)
+                        print(PRIV_STYLE + from_nickname + ": " + END_STYLE + TEXT_STYLE + msg + END_STYLE)
                         token = -1
                         send(TOKEN)
                 
@@ -145,12 +158,12 @@ def recive():
                             fixed_crc32 = crc32(last_msg_text, False)
                             last_msg = "7777:naoexiste;" + nickname + ";" + to_nickname + ";" + str(fixed_crc32) + ";" + last_msg_text
                             message_list = [last_msg] + message_list
-                            print("SYSTEM: O nó '" + to_nickname + "' recebeu a menssagem com erro. (msg: " + msg + ")")
+                            print(SYSTEM_STYLE + "SYSTEM: O nó '" + to_nickname + "' recebeu a menssagem com erro. (msg: " + msg + ")" + END_STYLE)
                             token = -1
                             send(TOKEN)
                         # Se o outro nó não existe:
                         elif header == "naoexiste":         
-                            print("SYSTEM: O nó '" + to_nickname + "' não existe. (msg: " + msg + ")")
+                            print(SYSTEM_STYLE + "SYSTEM: O nó '" + to_nickname + "' não existe. (msg: " + msg + ")" + END_STYLE)
                             token = -1
                             send(TOKEN)
                             
@@ -161,8 +174,8 @@ def recive():
 def handle_input():
     global message_list, nickname
     
-    print("\n# === === === === === === Chat === === === === === === #")
-    print("   ↓   ↓   ↓   ↓   ↓   ↓        ↓   ↓   ↓   ↓   ↓   ↓\n")
+    print(HEADER_STYLE + "\n# === === === === === === Chat === === === === === === #" + END_STYLE)
+    print(TITLE_STYLE + "   ↓   ↓   ↓   ↓   ↓   ↓        ↓   ↓   ↓   ↓   ↓   ↓\n" + END_STYLE)
     
     # Loop para ficar esperando por input de mensagens.
     while True:
@@ -174,14 +187,14 @@ def handle_input():
             new_message = "7777:naoexiste;" + nickname + ";" + to_nickname + ";" + str(crc32(text, True)) + ";" + text
         # Se for uma menssagem global:
         else:
-            new_message = "7777:naoexiste;" + nickname + ";TODOS;" + str(crc32(user_input, True)) + ";" + user_input
+            new_message = "7777:naoexiste;" + nickname + ";TODOS;" + str(crc32(user_input, False)) + ";" + user_input
         
         # Se tem espaço na lista de mensagens:
         if len(message_list) < 10:
             message_list.append(new_message)
         # Se não tem espaço na lista de mensagens:
         else:
-            print("SYSTEM: Message list is full. (msg: " + user_input + ")")
+            print(SYSTEM_STYLE + "SYSTEM: Message list is full. (msg: " + user_input + ")" + END_STYLE)
 
 # Essa função serve para enviar uma mensagem ao nodo referenciado no 'config.json'.
 def send(msg):
@@ -195,11 +208,11 @@ def send(msg):
 
 config()
 
-print("\n# === === === === === === Node === === === === === === #\n")
-print("  → Nickname: " + nickname)
-print("  → IP de destino: " + ip_destny)
-print("  → Delay de menssagem: " + str(delay) + " sec")
-print("  → Token generation status: " + str(gen_token))
+print(HEADER_STYLE + "\n# === === === === === === Node === === === === === === #\n" + END_STYLE)
+print(TITLE_STYLE + "    Nickname: " + END_STYLE + DARK_STYLE + nickname + END_STYLE)
+print(TITLE_STYLE + "    IP de destino: " + END_STYLE + DARK_STYLE + ip_destny + END_STYLE)
+print(TITLE_STYLE + "    Delay de menssagem: " + END_STYLE + DARK_STYLE + str(delay) + " sec" + END_STYLE)
+print(TITLE_STYLE + "    Token generation status: " + END_STYLE + DARK_STYLE + str(gen_token) + END_STYLE)
 
 if gen_token:
     handle_token_thread = threading.Thread(target=handle_token)
